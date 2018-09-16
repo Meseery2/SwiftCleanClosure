ee4aee6bc71053b6472d24eae9d28bb4a6dbc01b
Meseery2 <eng.m.elmeseery@gmail.com>
2018-09-16 13:24:31 +0200

    Clean Closure Target done.

---
+++ SwiftyClosure/SourceEditorCommand.swift
@@ -0,0 +1,60 @@
+//
+//  SourceEditorCommand.swift
+//  SwiftyClosure
+//
+//  Created by Mohamed EL Meseery on 9/16/18.
+//  Copyright © 2018 Meseery. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) -> Void {
+        var updatedLineIndexes = [Int]()
+
+        for lineIndex in 0 ..< invocation.buffer.lines.count {
+            let line = invocation.buffer.lines[lineIndex] as! NSString
+            do {
+                let regex = try NSRegularExpression(pattern: "\\{.*\\(.+\\).+in", options: .caseInsensitive)
+                let range = NSRange(0 ..< line.length)
+                let results = regex.matches(in: line as String, options: .reportProgress, range: range)
+                // 2. When a closure is found, clean up its syntax
+                _ = results.map { result in
+                    let cleanLine = line.remove(characters: ["(", ")"], in: result.range)
+
+                    updatedLineIndexes.append(lineIndex)
+                    invocation.buffer.lines[lineIndex] = cleanLine
+                }
+            } catch {
+                completionHandler(error as NSError)
+            }
+        }
+
+        // 3. If at least a line was changed, create an array of changes and pass it to the buffer selections
+        if !updatedLineIndexes.isEmpty {
+            let updatedSelections: [XCSourceTextRange] = updatedLineIndexes.map { lineIndex in
+                let lineSelection = XCSourceTextRange()
+                lineSelection.start = XCSourceTextPosition(line: lineIndex, column: 0)
+                lineSelection.end = XCSourceTextPosition(line: lineIndex, column: 0)
+                return lineSelection
+            }
+            invocation.buffer.selections.setArray(updatedSelections)
+        }
+
+        completionHandler(nil)
+    }
+
+}
+
+extension NSString {
+    // Remove the given characters in the range
+    func remove(characters: [Character], in range: NSRange) -> NSString {
+        var cleanString = self
+        for char in characters {
+            cleanString = cleanString.replacingOccurrences(of: String(char), with: "", options: .caseInsensitive, range: range) as NSString
+        }
+        return cleanString
+    }
+}
