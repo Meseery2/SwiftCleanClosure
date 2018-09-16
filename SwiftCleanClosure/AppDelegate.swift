4c866eef83b0213d78cc4f51b7beda9dd2e5729c
Meseery2 <eng.m.elmeseery@gmail.com>
2018-09-16 12:47:59 +0200

    Initial Commit

---
+++ SwiftCleanClosure/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftCleanClosure
+//
+//  Created by Mohamed EL Meseery on 9/16/18.
+//  Copyright © 2018 Meseery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftCleanClosure/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftCleanClosure
+//
+//  Created by Mohamed EL Meseery on 9/16/18.
+//  Copyright © 2018 Meseery. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
